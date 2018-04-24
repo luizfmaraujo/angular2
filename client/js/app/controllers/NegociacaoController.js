531cfc7ff8eade6243f1286fbafefab2345a6a1f
Luiz Fernando <luizfmaraujo@gmail.com>
2018-04-23 22:10:30 -0300

    Avançando no padrão de projetos

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,29 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputData = $('#data');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
+
+        this._listaNegociacoes = new Bind(new ListaNegociacoes(),
+            this._negociacoesView,'adiciona', 'esvazia');
+
         this._mensagemView = new MensagemView($('#mensagem'));
-        this._mensagemView.update(this._mensagem);
+
+        this._mensagem = new Bind(new Mensagem(),
+            this._mensagemView, 'texto');
     }
 
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView  .update(this._mensagem);
         this._limpaFormulario();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView(this._listaNegociacoes);
+        this._mensagem.texto = "Negociações apagadas com sucesso";
     }
     _criaNegociacao() {
         return new Negociacao(
